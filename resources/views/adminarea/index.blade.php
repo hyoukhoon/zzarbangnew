83ebb776f8ef9702930f5d4061520f85a3b8833d
escritos <escritoyane@gmail.com>
2025-05-26 17:02:05 +0900

    250526

--- resources/views/adminarea/index.blade.php
+++ resources/views/adminarea/index.blade.php
@@ -92,11 +92,14 @@
                         ?>
                         @foreach ($boards as $key => $board)
                             <tr>
-                                <td><a href="/boards/show/{{$board->num}}/{{$pagenumber}}">{{$board->subject}}</a>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-right-square-fill flex-shrink-0 me-2 rounded" viewBox="0 0 16 16">
+                                        <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4v8z"/>
+                                      </svg>
+                                    <a href="/boards/show/{{$board->num}}/{{$pagenumber}}">{{$board->subject}}</a>
                                     {{ $board->memo_cnt?"[".$board->memo_cnt."]":"" }}
                                 </td>
                                 <td>{{$board->cnt}}</td>
-                                <td>{{ disptime($board->reg_date) }}</td>
                             </tr>
                         @endforeach
                     </tbody>
