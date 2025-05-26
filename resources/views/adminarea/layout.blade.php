aafd006fb17570a303990613d21be6dc4381a975
escritos <escritoyane@gmail.com>
2025-05-26 16:55:48 +0900

    250526

--- resources/views/adminarea/layout.blade.php
+++ resources/views/adminarea/layout.blade.php
@@ -160,8 +160,8 @@ a{
 </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script src="/adminarea/js/scripts.js"></script>
+<script src="/adminjs/scripts.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-<script src="/adminarea/js/datatables-simple-demo.js"></script>
+<script src="/adminjs/datatables-simple-demo.js"></script>
 </body>
 </html>
