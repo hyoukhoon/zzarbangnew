767d8a3a1a48d83052a6b4b54617bff5136f7737
escritos <escritoyane@gmail.com>
2025-05-27 15:40:14 +0900

    250527

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -35,10 +35,10 @@ class AdminController extends Controller
     }
 
     public function elatest(){
-        $rs = Ozzal::where("multi","ozzal")->first();
+        $rs = Ozzal::where("multi","ozzal")->latest('site_reg_date')->paginate(20);
         echo "<pre>";
         print_r($rs);
-        print_r($rs->subject);
+        //print_r($rs->subject);
         echo "</pre>";
     }
 
