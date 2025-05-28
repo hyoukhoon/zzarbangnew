5a7b295ef2f358c1dea6d1b5a019a71e9af04053
escritos <escritoyane@gmail.com>
2025-05-28 10:46:26 +0900

    250528

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,11 +10,11 @@ use Illuminate\Validation\Rules\Password;
 class MainController extends Controller
 {
     public function index(){
-        $fromdate=date("Y/m/d", strtotime('-7 days'));
-        $nowdate=date("Y/m/d");
+        $fromdate=date("Y/m/d H:i:s", strtotime('-7 days'));
+        $nowdate=date("Y/m/d H:i:s");
         $hot = Ozzal::where("multi","ozzal")
-                    //->whereRaw("site_reg_date > $fromdate and site_reg_date < $nowdate")
-                    ->whereBetween("site_reg_date", [$fromdate, $nowdate])
+                    ->whereDate("site_reg_date", ">", $fromdate)
+                    ->whereDate("site_reg_date", "<", $nowdate)
                     ->orderBy("site_cnt","desc")
                     ->paginate(5);
         return view("index",['boards' => $hot]);
