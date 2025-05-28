d75168d2eb6ff3d0c78bc9a715ca080a98f00876
escritos <escritoyane@gmail.com>
2025-05-28 13:27:29 +0900

    250528

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,33 +11,13 @@ use pdphilip\elasticsearch\ES;
 class MainController extends Controller
 {
     public function index(){
-        $client = new ES();
-
-        $params = [
-            'index' => 'ozzal',
-            'body' => [
-                'query' => [
-                    'range' => [
-                        'site_reg_date' => [
-                            'gte' => '2025/05/18 00:00:00',
-                            'lte' => '2025/05/28 23:59:59',
-                            'format' => 'yyyy/MM/dd HH:mm:ss'
-                        ]
-                    ]
-                ]
-            ]
-        ];
-        
-        $response = $client->search($params);
-        dd($response);
-
 
         $fromdate = now()->subDays(10)->format('Y/m/d'); // 예: 2025/05/21 00:00:00
         $nowdate   = now()->format('Y/m/d');  
         $hot = Ozzal::where("multi","ozzal")
-                    //->where("site_reg_date", ">", $fromdate)
+                    ->where("site_reg_date", ">", $fromdate)
                     //->whereDate("site_reg_date", "<", $nowdate)
-                    ->whereBetween('site_reg_date', [$fromdate, $nowdate])
+                    //->whereBetween('site_reg_date', [$fromdate, $nowdate])
                     ->orderBy("site_cnt","desc")
                     ->paginate(5);
         //dd($fromdate, $nowdate);
--- app/Models/Ozzal.php
+++ app/Models/Ozzal.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use PDPhilip\Elasticsearch\Eloquent\Model;
+use pdphilip\elasticsearch\ElasticquentTrait;
 
 class Ozzal extends Model
 {
+
+    use ElasticquentTrait;
+    protected $indexName = 'ozzal';
+
     protected $connection = 'elasticsearch';
     protected $table = 'ozzal';
 
