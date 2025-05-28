a418996c63504fcfc6d1e7736094e8764c604080
escritos <escritoyane@gmail.com>
2025-05-28 13:00:01 +0900

    250528

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,10 +10,10 @@ use Illuminate\Validation\Rules\Password;
 class MainController extends Controller
 {
     public function index(){
-        $fromdate=now()->subWeek()->toDateString();
-        $nowdate=now()->toDateString();
+        $from = now()->subWeek()->format('Y/m/d H:i:s'); // 예: 2025/05/21 00:00:00
+        $to   = now()->format('Y/m/d H:i:s');  
         $hot = Ozzal::where("multi","ozzal")
-                    ->whereDate("site_reg_date", ">=", $fromdate)
+                    ->where("site_reg_date", ">=", $from)
                     //->whereDate("site_reg_date", "<", $nowdate)
                     //->whereBetween('site_reg_date', [$fromdate, $nowdate])
                     ->orderBy("site_cnt","desc")
